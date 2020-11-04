ed18b0bd9836ca59941dd9830c0aed6493927b0b
Isaiah Murray <murray.isaiah13@gmail.com>
2020-11-04 14:46:06 -0500

    Restyled and added modals

--- script.js
+++ script.js
@@ -1,61 +1,52 @@
+const films = document.getElementById("films");
+const listItem = document.createElement("li");
+const baseUrl = "https://ghibliapi.herokuapp.com/films";
 
-const films = document.getElementById('films');
-const listItem = document.createElement('li'); 
-const baseUrl = 'https://ghibliapi.herokuapp.com/';
-
-
-(async function(){
-    const movies = await fetch('https://ghibliapi.herokuapp.com/films')
-        .then(response => response.json());
-        
-    movies.forEach(movie => {
-      console.log(movie);
-
-     let divOne = document.createElement('div');
-     divOne.classList.add("card");
-     divOne.classList.add("col-sm-6");
-
-     let divTwo = document.createElement('div');
-     divTwo.classList.add("card-header");
-
-     let h2 = document.createElement('h4');
-     h2.innerHTML = movie.title;
-
-     divTwo.appendChild(h2);
-     divOne.appendChild(divTwo);
-
-     let divThree = document.createElement('div');
-     divThree.classList.add("card-body");
-
-     let p = document.createElement('p');
-     p.innerHTML = movie.description;
-
-     divThree.appendChild(p);
-     divOne.appendChild(divThree);
-
-     films.appendChild(divOne);
+ghibliFetch = () => {
+  fetch(baseUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      displayMovies(data);
     });
-  })();
+};
+ghibliFetch();
 
-  /*
-  Make cards
-  -create a div
-  -give it the class of "card col-md"
-
-  -create another div
-  -give it the class "card-header"
-  -create an h2 tag
-  -set the h2 tag to the movie title
-  -append the h2 tag to the card-header div
-  -append the card-header div to the card
-
-  -create another div
-  -give it the class "card-body"
-  -create a p tag
-  -set the p tag innerHTML to the movie description
-  -append the p tag to the card-body
-  -append the card-body to the card
-
-
-
-  */
+displayMovies = (movies) => {
+  for(let i = 0; i <= movies.length; i++){
+    let id = movies[i].id
+    let parentDiv = document.createElement("div");
+    let childDiv = document.createElement("div");
+    parentDiv.appendChild(childDiv);
+    films.appendChild(parentDiv);
+    childDiv.innerHTML = `<div class="card" style="width: 18rem;">
+    <div class="card-body" style="height: 14rem;">
+      <h5 class="card-title">${movies[i].title}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">Director: ${movies[i].director}</h6>
+      <h6 class="card-subtitle mb-2 text-muted">Producer: ${movies[i].producer}</h6>
+      <p class="card-text">${movies[i].release_date}</p>
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#${id}">
+  Description
+</button>
+    </div>
+  </div>
+  <div class="modal fade" id="${id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">${movies[i].title}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      ${movies[i].description}
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+  `;
+  };
+};
